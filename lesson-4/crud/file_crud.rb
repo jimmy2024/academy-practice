dd61a7895ce2c985998451d9aaa433398d26e966
jimmy2024 <114060342+jimmy2024@users.noreply.github.com>
2024-12-25 11:33:04 +0900

    fix file crud

--- lesson-4/crud/file_crud.rb
+++ lesson-4/crud/file_crud.rb
@@ -4,26 +4,31 @@ DB_PATH = "#{__dir__}/db.txt"
 TMP_FILE = "#{__dir__}/db.txt.tmp"
 
 def index
+  puts '======index======'
   File.foreach(DB_PATH) { |line| puts line }
 end
 
 def find(id)
+  puts "======find #{id}======"
   File.foreach(DB_PATH).with_index do |line, index|
-    puts line if index == id
-  return
+    if index == id - 1
+      puts line
+      return
+    end
   end
 end
 
 def where(pattern)
+  puts "======where #{pattern}========"
   File.foreach(DB_PATH) do |line|
-    puts line if /#{pattern}/ =~ line
+    puts line if line.include?(pattern)
   end
 end
 
 def update(id, text)
   f = File.open(TMP_FILE, 'w')
   File.foreach(DB_PATH).with_index do |line, index|
-    f.puts id == index ? text : line
+    f.print (id - 1 == index) ? "#{text}\n" : line
   end
   f.close
   File.delete(DB_PATH)
@@ -32,17 +37,17 @@ end
 
 def create(name)
   f = File.open(DB_PATH, 'a')
-  f.puts name
+  f.puts
+  f.print name
   f.close
 end
 
 def delete(id)
   f = File.open(TMP_FILE, 'w')
   File.foreach(DB_PATH).with_index do |line, index|
-    puts line unless id == index
+    f.print line unless (id - 1 == index)
   end
   f.close
   File.delete(DB_PATH)
   File.rename(TMP_FILE, DB_PATH)
 end
-
